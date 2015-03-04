23293cc31a5e1759ca80aee9bcbeba5673a586be
terran_j <terran_j@epitech.eu>
2015-03-04 18:25:46 +0100

    bientot la map

--- SDL/src/draw.c
+++ SDL/src/draw.c
@@ -5,7 +5,7 @@
 ** Login   <terran_j@epitech.net>
 **
 ** Started on  Wed Mar  4 15:15:36 2015 Julie Terranova
-** Last update Wed Mar  4 16:35:49 2015 Julie Terranova
+** Last update Wed Mar  4 17:58:01 2015 Julie Terranova
 */
 
 #include "lemisdl.h"
@@ -41,24 +41,37 @@ void    init(t_sdl *mine)
   mine->screen = NULL;
 }
 
+int	init_colors(t_sdl *mine)
+{
+  if ((mine->rasta = optimize_img("SDL/pictures/rasta.bmp")) == NULL)
+    return (-1);
+  if ((mine->japon = optimize_img("SDL/pictures/japon.bmp")) == NULL)
+    return (-1);
+  if ((mine->france = optimize_img("SDL/pictures/france.bmp")) == NULL)
+    return (-1);
+  return (0);
+}
+
 int     draw(int **map)
 {
   t_sdl mine;
 
   init(&mine);
-  //  XInitThreads();
+  // XInitThreads();
   if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
     return (-1);
   if ((mine.screen = SDL_SetVideoMode(1316, 800, 32, SDL_SWSURFACE)) == NULL)
     return (-1);
   SDL_WM_SetCaption("LemiPC", NULL);
-
   if ((mine.background = optimize_img("SDL/pictures/background.bmp")) == NULL)
     return (-1);
-
+  if (init_colors(&mine) == -1)
+    return (-1);
   if (SDL_Flip(mine.screen) == -1)
     return (-1);
-  show_map(map, &mine);
+
+  while (42) // tant que tu recois pas un msg comme quoi c'est fini
+    show_map(map, &mine);
 
   SDL_FreeSurface(mine.background);
   SDL_Quit();
